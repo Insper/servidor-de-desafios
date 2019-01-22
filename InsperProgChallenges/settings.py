0eeab584786bec3da5d4d455b22c9ae27c32b88f
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2019-01-22 15:52:41 -0200

    ALLOWED_HOSTS from production_settings.py

--- InsperProgChallenges/settings.py
+++ InsperProgChallenges/settings.py
@@ -33,8 +33,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'r*gl!)m0t=x##wt##w#4t=fl6-^kk8(smd547w*oi#77=*$69a'
 
-ALLOWED_HOSTS = []
-
 # LOGIN
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login'
