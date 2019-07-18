1eec123afe9e4ce126e8bc7cc1a7fd24beaa8a2c
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2019-07-18 17:27:02 -0300

    Changing admin model for Class: collapsing students and showing it as multiple checkboxes

--- course/admin.py
+++ course/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+from django.forms import CheckboxSelectMultiple
 from .models import Class
 
-admin.site.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name',)
+        }),
+        ('Students', {
+            'classes': ('collapse',),
+            'fields': ('students',),
+        }),
+    )
+
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name == 'students':
+            kwargs['widget'] = CheckboxSelectMultiple()
+            kwargs['help_text'] = ''
+
+        return db_field.formfield(**kwargs)
+
+admin.site.register(Class, ClassAdmin)
