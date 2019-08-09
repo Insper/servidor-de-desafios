081d8b4f5739c6d8d5c69c95ccec43a18d83bccb
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2019-08-09 17:44:19 -0300

    Adding batch students to class

--- challenges/management/commands/batch_add_users.py
+++ challenges/management/commands/batch_add_users.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 import pandas as pd
 from pathlib import Path
+from course.models import Class
 
 
 class Command(BaseCommand):
@@ -9,15 +10,18 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('filename', type=str, help='Indicates the file to be used')
+        parser.add_argument('course', type=str, help='Indicates the name of the course to add the students')
 
     def handle(self, *args, **kwargs):
         filename = Path(kwargs['filename'])
+        coursename = kwargs['course']
         if 'csv' in filename.suffix:
             df = pd.read_csv(filename)
         elif 'xls' in filename.suffix:
             df = pd.read_excel(filename)
         else:
             return
+        course = Class.objects.get(name=coursename)
         for user_data in df.iterrows():
             first_name = user_data[1]['Nome']
             try:
@@ -31,3 +35,8 @@ class Command(BaseCommand):
             if not users_same_name:
                 print('Creating user: {0}'.format(username))
                 User.objects.create_user(username=username, email=email, password=username, first_name=first_name, last_name=last_name)
+            user = User.objects.get(username=username)
+            if user not in course.students.all():
+                print('Adding {0} in {1}'.format(username, coursename))
+                course.students.add(user)
+        course.save()
