f4dee626d4b6496b17ba76f506c48056d5a0d616
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2019-09-27 14:09:30 -0300

    Logging ip addresses

--- InsperProgChallenges/settings.py
+++ InsperProgChallenges/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'challenges.middleware.LogIPMiddleware',
 ]
 
 ROOT_URLCONF = 'InsperProgChallenges.urls'
