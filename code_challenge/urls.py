c04b80f97abbd8efe22efa68630ca87e785165a9
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2021-02-26 18:11:17 -0300

    Add trailing slash to urls

--- code_challenge/urls.py
+++ code_challenge/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('interaction/', views.CodeInteractionListView.as_view(), name='code_interaction_list'),
     path('admin/interaction/', views.list_interactions_for, name='list_interactions_for'),
     path('<slug:slug>/', views.CodeChallengeView.as_view(), name='challenge'),
-    path('<slug:slug>/test-code', views.get_test_code, name='challenge_test_code'),
-    path('<slug:slug>/submission', views.CodeChallengeSubmissionListView.as_view(), name='submissions'),
-    path('<slug:slug>/submission/<int:submission_id>/code', views.CodeChallengeSubmissionCodeView.as_view(), name='submission_code'),
+    path('<slug:slug>/test-code/', views.get_test_code, name='challenge_test_code'),
+    path('<slug:slug>/submission/', views.CodeChallengeSubmissionListView.as_view(), name='submissions'),
+    path('<slug:slug>/submission/<int:submission_id>/code/', views.CodeChallengeSubmissionCodeView.as_view(), name='submission_code'),
 ]
